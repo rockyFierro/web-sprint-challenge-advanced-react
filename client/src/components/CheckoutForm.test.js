21b5efb17b5fb331d5b342a7f080ac39c7cd7860
rockyFierro <finnslighthouse@gmail.com>
2021-03-21 20:05:23 -0700

    tests complete, sanity checks succesfully failed.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -8,12 +8,14 @@ test("form header renders", () => {
   const {getByRole} = render(<CheckoutForm />)
   const header = getByRole('heading',{ queryFallbacks: true, level: 2})
   expect(header).toBeInTheDocument()
+  // expect(header).not.toBeInTheDocument() //sanity
 });
 
 test("form shows success message on submit with form  details", async () => {
-  const {getByRole} = render(<CheckoutForm/>)
-  const submit = getByRole('button', { name: submit})
-  userEvent.click(submit)
-  const submitted = await ( ()=> getByRole('heading', { name: submitted })
-  )
+  const {getByTestId, getByRole} = render(<CheckoutForm/>)
+  const button = getByRole('button', {queryFallbacks: true})
+  fireEvent.click(button)
+  const submitted = await waitFor( ()=> getByTestId('successMessage') )
+  expect(submitted).toBeInTheDocument()
+  // expect(submitted).not.toBeInTheDocument() //sanity
 });
